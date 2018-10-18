1e5c0c5acffff74d064e45f8f62a2ed453a7462c
Toshimitsu Takahashi <toshi@tilfin.com>
2018-10-19 00:27:12 +0900

    Add test

---
+++ test/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# json-log-stream tests
+#
+set -e
+
+tempfile=$(mktemp /tmp/jls_result.XXXXXXXXX)
+
+cd ${0%/*}
+
+node ../index.js v0_1.js < sample.log > $tempfile
+diff $tempfile v0_1_expected.log
+
+node ../index.js v0_2.js < sample.log > $tempfile
+diff $tempfile v0_2_expected.log
+
+rm $tempfile
---
+++ test/v0_1.js
@@ -0,0 +1,4 @@
+module.exports = async (item) => {
+  // filter
+  return item.bar
+}
